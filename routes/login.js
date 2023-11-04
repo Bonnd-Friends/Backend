ced0da5dc1f6460f29503e548a99447bc2b41fb5
Rohit Patil <rohitpradeeppatil@gmail.com>
2023-11-04 12:00:50 +0530

    Added Mailing System

---
+++ routes/login.js
@@ -0,0 +1,234 @@
+
+var express = require("express");
+var router = express.Router();
+const jwt = require('jsonwebtoken')
+const EmailRegistration = require("./../models/emailRegistration");
+const isAuthenticated = require('../middlewares/authMiddleware');
+const crypto = require("crypto")
+const {sendEmail, configureTransporter} = require("../utils/sendMail");
+
+let algorithm = "sha256"
+
+const transporter = configureTransporter({
+    email: process.env.NODEMAILER_EMAIL,
+    password: process.env.NODEMAILER_PASSWORD,
+});
+
+
+router.get("/get-login", (req, res)=>{
+    res.send(`
+    <form method="POST" action="/auth/login">
+        <input type="text" placeholder="username" name="username">
+        <input type="text" placeholder="password" name="password">
+        <input type="submit">Submit</input>
+    </form>
+    `)
+})
+
+
+router.get("/get-register", (req, res)=>{
+    res.send(`
+    <form method="POST" action="/auth/register">
+    <input type="text" placeholder="User name" name="username" required>
+    <input type="email" placeholder="Email" name="email" required>
+    <input type="password" placeholder="Password" name="password" required>
+    <input type="password" placeholder="Confirm Password" name="confirmPassword" required>
+    <input type="submit" value="Submit">
+</form>
+
+    `)
+})
+
+router.get("/get-reset", (req, res)=>{
+    res.send(`
+    <form method="POST" action="/auth/reset">
+    <input type="text" placeholder="User name" name="username" required>
+    <input type="email" placeholder="Email" name="email" required>
+    <input type="password" placeholder="Password" name="password" required>
+    <input type="password" placeholder="Confirm Password" name="confirmPassword" required>
+    <input type="submit" value="Submit">
+</form>
+
+    `)
+})
+
+
+router.get("/get-otp", (req, res)=>{
+    res.send(`
+        <form method="POST" action="/auth/generate-otp">
+        <input type="text" placeholder="User name" name="username" required>
+        <input type="submit" value="Submit">
+    </form>
+    `)
+})
+
+
+router.get("/verify-otp", (req, res)=>{
+    res.send(`
+        <form method="POST" action="/auth/verify-otp">
+        <input type="text" placeholder="User name" name="username" required>
+        <input type="number" placeholder="OTP" name="otp" required>
+        <input type="submit" value="Submit">
+    </form>
+    `)
+})
+
+
+
+router.post('/register', (req, res) => {
+    // Perform authentication logic, e.g., validate credentials
+    const { password, confirmPassword, username, email } = req.body;
+
+    if (password == confirmPassword){
+        const digest = crypto.createHash(algorithm).update(password).digest('hex')
+        const newuser= new EmailRegistration({
+            username: username,
+            email: email,
+            password: digest,
+            
+          });
+        
+          newuser.save()
+            .then((result)=>{
+            res.redirect("/auth/login")
+          })
+          .catch((err) =>{
+            console.log(err);
+            res.send(err)
+          });
+    }
+    else{
+        res.status(401).send("Both password are not same")
+    }
+    
+    
+});
+
+router.get('/user', isAuthenticated, (req, res)=>{
+    const token = req.cookies.token;
+    const resObject = {}
+    jwt.verify(token, process.env.JWT_ACCESS_KEY, (err, decoded) => {
+        if (err) {
+          console.error('JWT verification failed:', err.message);
+          res.status(401);
+        } else {
+          console.log('Decoded JWT payload:', decoded);
+          resObject.username = decoded.username;
+        }
+      });
+    res.send(resObject)
+})
+
+
+router.post('/generate-otp', async(req, res)=>{
+    const { username } = req.body;
+
+    const otp = Math.floor(Math.random() * (9999 - 0 + 1)) + 0;
+
+    try {
+        const result = await EmailRegistration.findOneAndUpdate({"username":username}, {"otp":otp});
+        sendEmail(result.email, {username:username, otp:otp}, transporter)
+    } catch (error) {
+        res.redirect('/auth/get-register')
+    }
+    
+    const interval = setInterval(async()=>{
+        try{
+            const updatedResult = await EmailRegistration.findOneAndUpdate({"username":username}, {$unset:{"otp":otp}});
+        }
+        catch(e){
+            console.log(e)
+        }
+    }, 60000)
+    res.send(`OTP Sent Successfully`)
+})
+
+router.post('/verify-otp', async(req, res)=>{
+    const { username, otp } = req.body;
+
+    try{
+        const result = await EmailRegistration.findOne({"username":username});
+        if(Number(otp) == Number(result.otp)){
+            const updatedResult = await EmailRegistration.findOneAndUpdate({"username":username}, {$unset:{"otp":result.otp}});
+            const payload = {username: username}
+            const token = jwt.sign(payload, process.env.JWT_ACCESS_KEY)
+            res.cookie('token', token, { httpOnly: true });
+            res.send("Login successful").status(200)
+        }
+    
+        else{
+            res.status(401).json({ message: 'Unauthorized' });
+        }
+    }
+    catch(e){
+        console.log(e);
+        res.send("Not registered yet").redirect("/auth/get-register");
+    }
+    
+})
+
+router.post('/reset', isAuthenticated, async(req, res)=>{
+    const { password, confirmPassword, username, email } = req.body;
+    // Complete this Function for reseting the password of the account
+    try{
+        const result = await EmailRegistration.findOne({"username":username});
+        if (password == confirmPassword){
+            const digest = crypto.createHash(algorithm).update(password).digest('hex')
+            
+            const result = await EmailRegistration.findOneAndUpdate({"username":username}, {"email":email, "username":username, "password":digest});
+            res.send("Password Updated successfully")
+        }
+        else{
+            res.status(401).send("Both password are not same")
+        }
+    }
+    catch(e){
+        console.log(e);
+        res.send("Not registered yet").redirect("/auth/get-register");
+    }
+    
+})
+
+
+router.post('/login', async(req, res) => {
+    // Perform authentication logic, e.g., validate credentials
+    const { username, password } = req.body;
+    
+
+    const digest = crypto.createHash(algorithm).update(password).digest('hex')
+    const result = await EmailRegistration.findOne({"username":username});
+    try{
+        if (username==result.email && digest==result.password){
+            //  Added jwt token in login
+            //  Not added in register
+            //  Also not added in middleware
+            const payload = {username: username}
+            const token = jwt.sign(payload, process.env.JWT_ACCESS_KEY)
+            res.cookie('token', token, { httpOnly: true });
+            res.send("Login successful").status(200)
+        }
+    
+        else{
+            res.status(401).json({ message: 'Unauthorized' });
+        }
+    }
+    catch(e){
+        console.log(e);
+        res.send("Not registered yet").redirect("/auth/get-register");
+    }
+    
+});
+
+
+router.get('/logout', isAuthenticated, (req, res) => {
+    // Clear the token cookie
+    
+    res.clearCookie("token")
+    res.send("logout successfully")
+    res.status(200)
+});
+
+
+
+
+module.exports=router;
---
+++ utils/sendMail.js
@@ -0,0 +1,57 @@
+
+const nodemailer = require('nodemailer');
+
+// Configure the nodemailer transporter
+function configureTransporter(emailAccount) {
+  return nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: emailAccount.email,
+      pass: emailAccount.password,
+    },
+  });
+}
+
+
+
+// Function to send emails
+function sendEmail(email, data, transporter) {
+  console.log(email);
+  if (email == undefined) {
+    console.log("Failed")
+    return
+  }
+
+  const mailOptions = {
+    
+    to: email,
+    subject: 'RCOEM-Friends] Please verify your device',
+    html: `
+    Hey ${data.username},<br/>
+    The OTP is ${data.otp}
+    `,
+  };
+
+
+
+  transporter.sendMail(mailOptions, (error, info) => {
+
+    if (error) {
+      console.log(`Error sending email to ${email}:`, error);
+    } else {
+      console.log(`Email sent to ${email}:`, info.response);
+    }
+
+  });
+}
+
+
+
+
+//sendEmail('khobragadesm_1@rknec.edu', transporter);
+//sendEmail('bagadiabm@rknec.edu',transporter)
+//sendEmail('munotkg@rknec.edu', transporter);
+
+//sendEmail('patilrp@rknec.edu', data)
+
+module.exports = {sendEmail, configureTransporter}
